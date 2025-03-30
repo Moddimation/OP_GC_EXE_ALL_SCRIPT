23164c70cb0b006e56b9199fc2dcbafe3e67da06
Moddimation <vakhidmytr06@gmail.com>
2025-03-30 11:52:19 +0200

    add

--- main.sh
+++ main.sh
@@ -1,28 +1,27 @@
       #  sudo pacman -S flatpak
 #        sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 #        sudo flatpak install flathub org.DolphinEmu.dolphin-emu -y
-
-#	echo "# Clearing old diles ..."
+	echo "# Clearing old files ..."
 #	sh clean.sh
-
-        alias filterDelete='find . -type f \( -iname "*.gct" -or -iname "*.gfn" -or -iname "*.bnr" -or -iname "*.h4m" -or -iname "*.sni" -or -iname "*.gsf" -or -iname "*.zsd" -or -iname "*.thp" -or -iname "*.mpc" -or -iname "*.bmd" -or -iname "*.fpk" -or -iname "*.viv" -or -iname "*.ngc" -or -iname "*.div" -or -iname "*.vid" -or -iname "*.vp*" -or -iname "*.sp" -or -iname "*.str" -or -iname "*.mus" -or -iname "*.flo" -or -iname "*.exa" -or -iname "*.ssd" -or -iname "*.sbf" -or -iname "*.spe" -or -iname "*.dat" -or -iname "*.sdt" -or -iname "*.lmp" -or -iname "*.feb" -or -iname "*.bin" -or -iname "*.dat" -or -iname "*.obj" -or -iname "*.lfb" -or -iname "*.med" -or -iname "*.samp"  -or -iname ".bnk" -or -iname "*.dsp" \) -exec rm {} -f \;'
-        alias filterTextFiles='grep -Evlis "__start|msl_c|MetroTRK|jsystem|#!/bin|\b[a-zA-Z]{6,}\.(cpp|hpp|a|o|c|h)\b|text section layout" *.map *.txt *.ini *.xml *.cfg | xargs -I{} rm -f {} 2>/dev/null'
-        alias filterFind='grep -lrEis "ppceabi|metrotrk|metrowerks|msl_c|text section layout|\b[a-zA-Z]{6,}\.(cpp|hpp|a|o|c|h)\b" . | xargs -I{} mv "{}"'
+	rm index.* *.zip* *.rvz* -f
+        alias filterDelete='find . -type f \( -iname "*.gct" -or -iname "*.gfn" -or -iname "*.bnr" -or -iname "*.h4m" -or -iname "*.sni" -or -iname "*.gsf" -or -iname "*.zsd" -or -iname "*.thp" -or -iname "*.mpc" -or -iname "*.bmd" -or -iname "*.fpk" -or -iname "*.viv" -or -iname "*.ngc" -or -iname "*.div" -or -iname "*.vid" -or -iname "*.vp*" -or -iname "*.sp" -or -iname "*.str" -or -iname "*.mus" -or -iname "*.flo" -or -iname "*.exa" -or -iname "*.ssd" -or -iname "*.sbf" -or -iname "*.spe" -or -iname "*.dat" -or -iname "*.sdt" -or -iname "*.lmp" -or -iname "*.feb" -or -iname "*.bin" -or -iname "*.dat" -or -iname "*.obj" -or -iname "*.lfb" -or -iname "*.med" -or -iname "*.samp"  -or -iname ".bnk" -or -iname "*.dsp" -or -iname "*.gsh" -or -iname "*.fsh" -or -iname "*.vsh" -or -iname "*.big" -or -iname *.abg -or -iname "*.bad" -or -iname "*.add" -o -iname "*.adb" -o -iname "*.fs" \) -exec rm {} -f \;'
+#        alias filterTextFiles='grep -Elis "__start|msl_c|MetroTRK|jsystem|#!/bin|\b[a-zA-Z]{6,}\.(cpp|hpp|a|o|c|h)\b([\"\'> \n])|text section layout" *.map *.txt *.ini *.xml *.cfg | xargs -I{} rm -f {} 2>/dev/null'
+        alias filterFind='grep -Elris "ppceabi|metrotrk|metrowerks|msl_c|text section layout|([a-z]|[A-Z]){5,}\.(cpp|hpp|a|o|c|h)\b$" . | xargs -I{} mv "{}"'
+	alias filterExt='find . -type f \( -iname "*.map*" -o -iname "*.rel*" -o -iname "*.elf*" -o -iname "*.exe*" -o -iname "*.txt" -o -iname "*.dol" -o -iname "*.sym" -o -iname "*.rsym" -o -iname "*.lua" -o -iname "*.rso" -o -iname "*.csv" -o -iname "*.dlf" -o -iname "*.sh" -o -iname "*.gci" -o -iname "*.sav*" -o -iname "*.tdf" -o -iname "*.inf" -o -iname "*.bat" -o -iname "*.scc" -o -iname "*.cfg" -o -iname "*.s" -o -iname "*.c" -o -iname "*.h" -o -iname "*.cpp" -o -iname "*.hpp" -o -iname "*.o" -o -iname "*.a" -o -iname "*.flb" -o -iname "*.xml" -o -iname "*.so" -o -iname "*.dll" -o -iname "*.*log*" -o -iname "*.ini" -o -iname "*.yml" -o -iname "*.yaml" -o -iname "*.json" -or -iname "*.py" -or -iname "*.exe" -o -iname "*makefile*" -o -iname "*cmake*" -o -iname "*.sln" -o -iname "*vsproj" -o -iname "*.mak" -o -iname "*.mk" \) -exec mv {}'
+        alias filterTextFiles='grep -Elisv "__start|msl_c|MetroTRK|jsystem|#!/bin|([a-z]|[A-Z]){5,}\.(cpp|hpp|a|o|c|h)\b$" *.ini *.Ini *.InI *.INI *.INi *.iNI *.iNi *.mAp *.tXt *.map *.Map *.MAP *.MAp *.MaP *.mAP *.maP *.Txt *.TXt *.TXT *.tXT *.txT *.TxT *.txt *.inf *.inF *.Inf *.INF *.InF *.INf *.iNF *.InF *.iNf *.log *.LOG *.loG *.LOg *.LoG *.lOg *.lOG *.xml *.XML *.Xml *.xML *.XMl *.XmL *.xML *.cfg *.cFg *.CFG *.CFg *.cFg *.CfG | xargs -I{} rm -f {} 2>/dev/null'
+#	alias filterFind='find . -type f -exec grep -Elis "ppceabi|metrotrk|metrowerks|msl_c|text section layout|[a-zA-Z]{6,}\.(cpp|hpp|a|o|c|h)\b$" {} \; | xargs -I{} mv "{}"'
 #	alias dolphinTool='flatpak run --command="dolphin-tool" --filesystem host org.DolphinEmu.dolphin-emu'
 	alias dolphinTool='dolphin-tool'
-
         echo "# Getting index ..."
-        wget -q --show-progress https://myrient.erista.me/files/Redump/Nintendo%20-%20GameCube%20-%20NKit%20RVZ%20%5Bzstd-19-128k%5D/
+        wget -q https://myrient.erista.me/files/Redump/Nintendo%20-%20GameCube%20-%20NKit%20RVZ%20%5Bzstd-19-128k%5D/
         export link_prefix="https://myrient.erista.me/files/Redump/Nintendo%20-%20GameCube%20-%20NKit%20RVZ%20%5Bzstd-19-128k%5D"
         echo "# OK, now index for filenames, download and process everything ..."
         cat index.html | grep .zip | sed 's/\" title.*//' | sed 's/.*href=\"//' | while read -r zip_file; do
           echo ">> Fetching next game ..." && \
-          wget -q --progress=dot "$link_prefix/$zip_file" & PID=$! && \
-
+          wget -q --show-progress "$link_prefix/$zip_file" & PID=$! && \
 #	  sleep 1 && \
 	  filename=$(ls *.zip 2>/dev/null | head -n 1) && \
 	  file_name="${filename%.zip}" && \
-
 	  if [ -z "$filename" ]; then
    	    while [ -z "$filename" ]; do
 #	      sleep 1
@@ -30,17 +29,17 @@
             done;
 	    file_name="${filename%.zip}";
           fi && \
-
           if ! [ -d "$file_name" ]; then
-	    echo "   Name: $file_name";
+#	    echo "   Name: $file_name";
 	    wait $!;
           else
-            echo "<< $file_name already exists, continue ...";
+            echo "<< $file_name already exists!";
 	    pkill wget 2>/dev/null;
      	    rm "$filename";
+            echo "   Continue, current size:";
+            du -sh .;
             continue;
 	  fi && \
-
           filename=$(ls *.zip | head -n 1) && \
           file_name="${filename%.zip}" && \
           mkdir "$file_name" && \
@@ -49,53 +48,20 @@
           rm "$filename" && \
           rvz_file=$(ls *.rvz | head -n 1) && \
           echo " # Extracting game ..." && \
-          dolphinTool extract -i "$rvz_file" -o "$file_name/" -q && \
+          dolphinTool extract -i "$rvz_file" -o "$file_name/" -q || true && \
           rm "$rvz_file" && \
           cd "$file_name" && \
           echo " # Run filter ..." && \
           rm -rf ../tmp && \
           mkdir -p ../tmp && \
-#          find . -type f -iname "*debug*" -exec mv "{}" "../tmp/" \; && \
-          find . -type f -iname "*.map*" -exec mv "{}" "../tmp/" \; && \
-          find . -type f -iname "*.rel*" -exec mv "{}" "../tmp/" \; && \
-          find . -type f -iname "*.elf*" -exec mv "{}" "../tmp/" \; && \
-          find . -type f -iname "*.txt*" -exec mv "{}" "../tmp/" \; && \
-          find . -type f -iname "*.dol*" -exec mv "{}" "../tmp/" \; && \
-#          find . -type f -iname "*.ini*" -exec mv "{}" "../tmp/" \; && \
-          find . -type f -iname "*.sym*" -exec mv "{}" "../tmp/" \; && \
-#          find . -type f -iname "main.*" -exec mv "{}" "../tmp/" \; && \
-          find . -type f -iname "*.flb" -exec mv "{}" "../tmp/" \; && \
-          find . -type f -iname "*.rsym*" -exec mv "{}" "../tmp/" \; && \
-          find . -type f -iname "*.lua*" -exec mv "{}" "../tmp/" \; && \
-          find . -type f -iname "*.rso*" -exec mv "{}" "../tmp/" \; && \
-#	  find . -type f -iname "*.scc" -exec mv "{}" "../tmp/" \; && \
-#          find . -type f -iname "*.cfg*" -exec mv "{}" "../tmp/" \; && \
-          find . -type f -iname "*.csv*" -exec mv "{}" "../tmp/" \; && \
-          find . -type f -iname "*.gci*" -exec mv "{}" "../tmp/" \; && \
-          find . -type f -iname "*.tdf" -exec mv "{}" "../tmp/" \; && \
-          find . -type f -iname "*.sav*" -exec mv "{}" "../tmp/" \; && \
-          find . -type f -iname "*.div" -exec mv "{}" "../tmp/" \; && \
-          find . -type f -iname "*.sh" -exec mv "{}" "../tmp" \; && \
-          find . -type f -iname "*.bat" -exec mv "{}" "../tmp/" \; && \
-          find . -type f -iname "*.dlf" -exec mv "{}" "../tmp/" \; && \
-          find . -type f -iname "*.sh" -exec mv "{}" "../tmp/" \; && \
-#          find . -type f -iname "*.inf" -exec mv "{}" "../tmp/" \; && \
-          find . -type f -iname "*.xml*" -exec mv "{}" "../tmp/" \; && \
-          find . -type f -iname "*.s" -exec mv "{}" "../tmp/" \; && \
-          find . -type f -iname "*.a" -exec mv "{}" "../tmp/" \; && \
-          find . -type f -iname "*.c" -exec mv "{}" "../tmp/" \; && \
-          find . -type f -iname "*.h" -exec mv "{}" "../tmp/" \; && \
-          find . -type f -iname "*.cpp" -exec mv "{}" "../tmp/" \; && \
-          find . -type f -iname "*.hpp" -exec mv "{}" "../tmp/" \; && \
-          find . -type f -iname "*.o" -exec mv "{}" "../tmp/" \; && \
-#          find . -type f -iname "*.*log*" -exec mv "{}" "../tmp/" \; && \
+          filterExt "../tmp/" \; && \
           find . -type f \( -iname *.iso -or -iname *.gcm -or -iname *.tgc \) | while read -r possible_archive_file; do
               sub_file_name=$(basename "$possible_archive_file" | sed 's/\.[^.]*$//') && \
               echo "  > Found sub-game: '$file_name/$sub_file_name'" && \
               mkdir "../tmp/$sub_file_name" -p && \
               mkdir "$sub_file_name" -p && \
               echo "  # Extracting sub..." && \
-              dolphinTool extract -i "$possible_archive_file" -o "$sub_file_name" -q > nul && \
+              dolphinTool extract -i "$possible_archive_file" -o "$sub_file_name" -q 2>/dev/null || true && \
               rm "$possible_archive_file" && \
               if [ -z "$(ls -A "$sub_file_name/")" ]; then
                 echo "  ! No data found, continue";
@@ -104,91 +70,54 @@
                 continue; 
               fi && \
               echo "  # Run filter..." && \
-              find "$sub_file_name" -type f -iname "*debug*" -exec mv "{}" "../tmp/$sub_file_name/" \; && \
-              find "$sub_file_name" -type f -iname "*.map*" -exec mv "{}" "../tmp/$sub_file_name/" \; && \
-              find "$sub_file_name" -type f -iname "*.rel*" -exec mv "{}" "../tmp/$sub_file_name/" \; && \
-              find "$sub_file_name" -type f -iname "*.elf*" -exec mv "{}" "../tmp/$sub_file_name/" \; && \
-              find "$sub_file_name" -type f -iname "*.txt*" -exec mv "{}" "../tmp/$sub_file_name/" \; && \
-              find "$sub_file_name" -type f -iname "*.dol*" -exec mv "{}" "../tmp/$sub_file_name/" \; && \
-              find "$sub_file_name" -type f -iname "*.sym*" -exec mv "{}" "../tmp/$sub_file_name/" \; && \
-              find "$sub_file_name" -type f -iname "*.rsym*" -exec mv "{}" "../tmp/$sub_file_name/" \; && \
-              find "$sub_file_name" -type f -iname "main.*" -exec mv "{}" "../tmp/$sub_file_name/" \; && \
-              find "$sub_file_name" -type f -iname "*.lua*" -exec mv "{}" "../tmp/$sub_file_name/" \; && \
-              find "$sub_file_name" -type f -iname "*.rso*" -exec mv "{}" "../tmp/$sub_file_name/" \; && \
-              find "$sub_file_name" -type f -iname "*.csv*" -exec mv "{}" "../tmp/$sub_file_name/" \; && \
-              find "$sub_file_name" -type f -iname "*.dlf" -exec mv "{}" "../tmp/$sub_file_name/" \; && \
-	      find "$sub_file_name" -type f -iname "*.sh" -exec mv "{}" "../tmp/$sub_file_name/" \; && \
-              find "$sub_file_name" -type f -iname "*.gci*" -exec mv "{}" "../tmp/$sub_file_name/" \; && \
-              find "$sub_file_name" -type f -iname "*.sav*" -exec mv "{}" "../tmp/$sub_file_name/" \; && \
-              find "$sub_file_name" -type f -iname "*.tdf" -exec mv "{}" "../tmp/$sub_file_name/" \; && \
-              find "$sub_file_name" -type f -iname "*.inf" -exec mv "{}" "../tmp/$sub_file_name/" \; && \
-              find "$sub_file_name" -type f -iname "*.bat" -exec mv "{}" "../tmp/$sub_file_name/" \; && \
-	      find "$sub_file_name" -type f -iname "*.scc" -exec mv "{}" "../tmp/$sub_file_name/" \; && \
-              find "$sub_file_name" -type f -iname "*.cfg*" -exec mv "{}" "../tmp/$sub_file_name/" \; && \
-              find "$sub_file_name" -type f -iname "*.sh" -exec mv "{}" "../tmp/$sub_file_name/" \; && \
-              find "$sub_file_name" -type f -iname "*.s" -exec mv "{}" "../tmp/$sub_file_name/" \; && \
-              find "$sub_file_name" -type f -iname "*.c" -exec mv "{}" "../tmp/$sub_file_name/" \; && \
-              find "$sub_file_name" -type f -iname "*.flb" -exec mv "{}" "../tmp/$sub_file_name/" \; && \
-              find "$sub_file_name" -type f -iname "*.a" -exec mv "{}" "../tmp/$sub_file_name/" \; && \
-              find "$sub_file_name" -type f -iname "*.cpp" -exec mv "{}" "../tmp/$sub_file_name/" \; && \
-              find "$sub_file_name" -type f -iname "*.h" -exec mv "{}" "../tmp/$sub_file_name/" \; && \
-              find "$sub_file_name" -type f -iname "*.hpp" -exec mv "{}" "../tmp/$sub_file_name/" \; && \
-              find "$sub_file_name" -type f -iname "*.o" -exec mv "{}" "../tmp/$sub_file_name/" \; && \
-              find "$sub_file_name" -type f -iname "*.xml*" -exec mv "{}" "../tmp/$sub_file_name/" \; && \
- #             find "$sub_file_name" -type f -iname "*.*log*" -exec mv "{}" "../tmp/$sub_file_name/" \; && \
-              find "$sub_file_name" -type f -iname "*.ini*" -exec mv "{}" "../tmp/$sub_file_name/" \; && \
-
+              cd "$sub_file_name" && \
+	      filterExt "../../tmp/$sub_file_name/" \; && \
 	      echo "  # Post processing ..." && \
-
-	      filrerFind "../tmp/$sub_file_name/" && \
-
-	      ignored=$(find "$sub_file_name" -type f | awk -F. '{if (NF>1) print $NF}' | sort -u | paste -sd "," - ) && \
-
-              cd "../tmp/$sub_file_name" && \
+	      filterFind "../../tmp/$sub_file_name/" && \
+#	      echo "no" && \
+	      ignored=$(find . -type f | awk -F. '{if (NF>1) print $NF}' | sort -u | paste -sd "," - ) && \
+              cd "../../tmp/$sub_file_name" && \
               filterTextFiles && \
               filterDelete && \
-              cd "../$file_name" && \
+              cd "../../$file_name" && \
               rm -rf "$sub_file_name" && \
               if [ -z "$(ls -A "../tmp/$sub_file_name")" ]; then
                 echo "  ! No code data found, continuing";
                 rmdir "../tmp/$sub_file_name";
                 continue;
               fi && \
-
               echo "  < Finished '$file_name/$sub_file_name'" && \
               echo "    Ignored:" && \
               echo $ignored && \
               echo "    Found:" && \
-              ls ../tmp/$sub_file_name -m
+              ls "../tmp/$sub_file_name" -m
           done && \
-
 	  echo " # Post processing ..." && \
-
           filterFind "../tmp/" && \
-
+#	  echo "no" && \
           if [ -z "$(ls -A ../tmp/)" ]; then 
-            echo " !  No code data found, continue";
+            echo " ! No code data found.";
+            echo "   Continuing, current size:";
+            du -sh .;
             rm -rf ../tmp/*;
             cd ..;
             rm -rf "$file_name";
             continue;
           fi && \
-
+#	  echo "nooo" && \
           ignored=$(find . -type f | awk -F. '{if (NF>1) print $NF}' | sort -u | paste -sd "," - ) && \
-
           rm -rf ./* && \
           cd ../tmp && \
-
           rm -f *lang* *locale* *CONTENT* && \
+#	  echo "noooooo" && \
           filterTextFiles && \
+#	  echo "yes" && \
           filterDelete && \
           ls | xargs -I{} mv "{}" "../$file_name" && \
           rm -rf "./*" && \
-
           echo "<< Finished '$file_name'" && \
           echo "   Ignored:" && \
 	  echo $ignored && \
-
           cd .. && \
           echo "   Found:" && \
           ls "$file_name" -m && \
